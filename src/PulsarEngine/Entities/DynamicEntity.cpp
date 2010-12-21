13dde20f3bba8f56052c48017cbb3b1c21fb76fd
Felix Müller <feelx88@googlemail.com>
2010-12-21 12:26:05 +0100

    Set the License of the engine to te LGPL.

--- src/PulsarEngine/Entities/DynamicEntity.cpp
+++ src/PulsarEngine/Entities/DynamicEntity.cpp
@@ -1,3 +1,22 @@
+ /*
+    Copyright 2010 Felix Müller
+
+    This file is part of the PulsarEngine.
+
+    The PulsarEngine is free software: you can redistribute it and/or modify
+    it under the terms of the Lesser GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The PulsarEngine is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Lesser GNU General Public License for more details.
+
+    You should have received a copy of the Lesser GNU General Public License
+    along with The PulsarEngine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "DynamicEntity.h"
 
 using namespace pulsar;
